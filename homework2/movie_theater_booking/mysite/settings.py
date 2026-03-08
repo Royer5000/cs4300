1c6dc80bdf7a91ac2ab4a3a7988f361c18c91399
Royer5000 <royerjr21@gmail.com>
2026-03-08 14:48:55 +0000

    removed gyp==0.1

--- homework2/movie_theater_booking/mysite/settings.py
+++ homework2/movie_theater_booking/mysite/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
